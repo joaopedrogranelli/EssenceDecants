6b34e77b9359d37cccfddbe90f2740c9f5f5143a
João Pedro Granelli <104399325+joaopedrogranelli@users.noreply.github.com>
2024-05-25 20:26:32 -0300

    Update geral no projeto
    Projeto modificado de DoctorCare para Essence Decants (Projeto para outro cliente)

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
+const navigation = document.querySelector("nav");
+const backToTopButton = document.getElementById("backToTopButton");
 
+window.addEventListener("scroll", scrolling);
+scrolling();
 
-
-window.addEventListener('scroll', scrolling)
-scrolling()
-
-function scrolling () {
-  showNavOnScroll() 
-  showBackToTopButton()
-  activateMenuAtCurrentSection(home)
-  activateMenuAtCurrentSection(services)
-  activateMenuAtCurrentSection(about)
-};
+function scrolling() {
+  showNavOnScroll();
+  showBackToTopButton();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+}
 
 function activateMenuAtCurrentSection(section) {
-    const targetLine = scrollY + innerHeight / 2
+  const targetLine = scrollY + innerHeight / 2;
 
-    const sectionTop = section.offSetTop
-    const sectionHeight = section.offSetHeight
-    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  const sectionTop = section.offsetTop; // Corrected typo
+  const sectionHeight = section.offsetHeight; // Corrected typo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
 
-    const sectionEndsAt = sectionTop + sectionHeight
-    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
 
-    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
 
-    const sectionId = section.getAttribute('id')
-    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
 
-    menuElement.classList.remove('active')
-    if (sectionBoundaries) {
-        menuElement.classList.add('active')
-        
-    }
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
-function showNavOnScroll () {
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
 }
 
-function showBackToTopButton () {
-    if (scrollY > 1380) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
+function showBackToTopButton() {
+  if (scrollY > 1380) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
 }
 
-function openMenu () {
-    document.body.classList.add('menu-expanded')
+function openMenu() {
+  document.body.classList.add("menu-expanded");
 }
 
-function closeMenu () {
-    document.body.classList.remove('menu-expanded')
-};
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
 
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 1550,
-}).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about header, #about .content p, #about .content img, #contact');
+  origin: "top",
+  distance: "30px",
+  duration: 1550,
+}).reveal(
+  "#home, #home img, #home .stats, #services, #services header, #services .card, #about header, #about .content p, #about .content img, #contact"
+);
